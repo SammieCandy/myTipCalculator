7375e1c24ddad723fd2e378af3989092059790f4
SammieCandy <samueloluyemi@icloud.com>
2022-08-16 06:13:41 +0100

    Add files via upload
    Tip Calculator Upgrade

--- tipcalc.js
+++ tipcalc.js
@@ -1,18 +1,45 @@
-console.log('calculate')
-document.querySelector('#calculator').onclick = calculate;
-const form  = document.querySelector('#calculator')
+// function calculate() {
+//     document.querySelector('#form').onclick = function calculate () {
+//         console.log('percentage')
+//         console.log('amount')
+//         console.log(form.elements['percentage'].value)
+//         console.log(form.elements['tip'].value)
+//         console.log(form.elements['total'].value)
+//         let amount = Number(document.querySelector('amount').value);
+//         let percentage = document.querySelector('percentage').value;
+//         let tipValue = amount * (percentage / 100);
+//         let totalBill = amount + tipValue;
+    
+//         console.log(total)
+//         let tip = document.querySelector('#tip')
+//         let total = document.querySelector('#total')
+    
+//         tip.value = tipValue.toFixed(2);
+//         total.value = totalBill.toFixed(2);
+//     }
+// }
 
-function calculate () {
-    console.log('percentage')
-    console.log('amount')
-    console.log(form.elements['percentage'].value)
-    console.log(form.elements['tip'].value)
-    console.log(form.elements['total'].value)
-    let amount = document.querySelector('#amount').value;
-    let percentage = document.querySelector('#percentage').value;
-    let tip = amount * (percentage / 100);
-    let total = Number(amount) + tip;
 
+var x = document.getElementById("calculator");
+var y = document.getElementById("addup");
+var z = document.getElementById("clear");
 
-}
+y.addEventListener("click", function() {
+
+            let amount = Number(document.getElementById("amount").value);
+            let percentage = document.getElementById("percentage").value;
+            let tipValue = amount * (percentage/100);
+            let totalBill = amount + tipValue;
+        
+            let tip = document.getElementById("tip");
+            let output = document.getElementById("output");
+        
+            tip.value = tipValue.toFixed(2);
+            output.value = totalBill.toFixed(2);
+
+            z.addEventListener('click', function() {
+                tip.value = '';
+                output.value = '';
+            });
+});
 
